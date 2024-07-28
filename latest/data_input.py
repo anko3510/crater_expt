adcb488b24689b3a2c032b78929bd677a87c438d
mkto <mkto@outlook.com>
2024-07-28 14:04:17 +0900

    update

---
+++ latest/data_input.py
@@ -0,0 +1,83 @@
+# データ入力とプロット用スクリプト
+
+# 衝突体の高さを入力
+while True:
+    print("衝突体の高さ(cm)を半角数字で入力してEnterを押してください.")
+    while True:
+        try:
+            height = float(input())  # 高さを入力
+        except ValueError:
+            print("不正な入力値です. 数字を入力してください.")
+        else:
+            break
+
+    print(f"衝突体の高さは {height:5.1f} cm です.")
+    print("この入力値で正しいですか? (y/n)")
+    while True:
+        check = input().strip().lower()
+        if check in ("y", "n"):
+            break
+        print("不正な入力です. yまたはnで答えてください.")
+    
+    if check == "y":
+        break
+
+print()
+print("衝突体を落としてクレーターを作成してください．")
+print("クレーターの画像を撮影してください.")
+print("撮影しましたか? (y)")
+while True:
+    check = input().strip().lower()
+    if check == "y":
+        break
+    print("不正な入力です. yで答えてください.")
+print()
+
+# クレーターの弦の長さを入力
+while True:
+    print("1つ目の弦の長さ(cm)を半角数字で入力してEnterを押してください.")
+    while True:
+        try:
+            chord1 = float(input())  # 1つ目の弦の長さを入力
+        except ValueError:
+            print("不正な入力値です. 数字を入力してください.")
+        else:
+            break
+
+    print("2つ目の弦の長さ(cm)を半角数字で入力してEnterを押してください.")
+    while True:
+        try:
+            chord2 = float(input())  # 2つ目の弦の長さを入力
+        except ValueError:
+            print("不正な入力値です. 数字を入力してください.")
+        else:
+            break
+
+    print("3つ目の弦の長さ(cm)を半角数字で入力してEnterを押してください.")
+    while True:
+        try:
+            chord3 = float(input())  # 3つ目の弦の長さを入力
+        except ValueError:
+            print("不正な入力値です. 数字を入力してください.")
+        else:
+            break
+
+    print(f"弦の長さは {chord1:5.2f} cm, {chord2:5.2f} cm, {chord3:5.2f} cm です.")
+    print("この入力値で正しいですか? (y/n)")
+    while True:
+        check = input().strip().lower()
+        if check in ("y", "n"):
+            break
+        print("不正な入力です. yまたはnで答えてください.")
+    
+    if check == "y":
+        break
+
+# クレーターの直径とその4乗を計算
+D = (2.0 * chord1 * chord2 * chord3) / (2.0 * (chord1**2 * chord2**2 + chord2**2 * chord3**2 + chord3**2 * chord1**2) - chord1**4 - chord2**4 - chord3**4)**0.5
+print()
+print(f"クレーターの直径は{D:5.2f} cm です. 直径の4乗は{D**4:5.2e} cm^4 です.")
+
+# データをファイルに書き込む
+with open("./data.dat", "a", encoding="utf-8") as newest:
+    newest.write(f"{height}, {chord1}, {chord2}, {chord3}, {D}, {D**4}\n")
---
+++ latest/data_plot.py
@@ -0,0 +1,55 @@
+# 必要なライブラリのインポート
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 定数の設定
+g = 9.8         # 重力加速度 [m/s^2]
+m = 13.8e-3     # 鉄球の質量 [kg]
+
+# データのインポート
+Data = np.loadtxt("data.dat", delimiter=",")  # データを読み込み
+Height = Data[:,0]      # 高さ [cm]（最初の列）
+R4 = Data[:,5] / 16     # 半径の4乗 [cm^4]（6列目のデータを16で割る）
+
+# プロットの設定
+## フォント設定
+plt.rcParams["font.family"] = "IPAexGothic"  # 日本語フォントの設定
+plt.rcParams["mathtext.fontset"] = "cm"      # 数式フォントの設定
+
+## 図と軸の設定
+fig, ax1 = plt.subplots()
+ax1.set_xscale("log")  # X軸を対数スケールに設定
+ax1.set_yscale("log")  # Y軸を対数スケールに設定
+ax1.set_xlim(10, 200)  # X軸の範囲を設定
+ax1.set_ylim(1**4, 4**4)  # Y軸の範囲を設定
+ax1.set_xlabel(r"高さ $h$ [$\mathrm{cm}$]")  # X軸ラベルを設定
+ax1.set_ylabel(r"半径の4乗 $r^4$ [$\mathrm{cm^4}$]")  # Y軸ラベルを設定
+ax1.grid(True, which="both", linestyle=":")  # グリッドを表示
+
+# 第2軸の追加
+ax2 = ax1.secondary_xaxis("top")  # 上部に第2のX軸を追加
+ax2.set_xlabel(r"ぶつかったときのエネルギー $E$ [$\mathrm{J}$] (小←→大)")
+xticks = ax2.get_xticks()
+ax2.set_xticks(xticks, [f"{x:.3g}" for x in (m * g * xticks / 100)])
+
+# データのプロット
+ax1.scatter(Height[-1],   R4[-1],   c = "C0", label="あなたの記録")  # 最後のデータ点（あなたの記録）
+ax1.scatter(Height[0:-1], R4[0:-1], c = "C1", label="他の人の記録")  # その他のデータ点
+
+# 目安の線のプロット
+meyasu_x = np.linspace(5, 200, 100)
+mayasu_y0 = 1e1  * meyasu_x
+meyasu_y1 = 1e0  * meyasu_x
+meyasu_y2 = 1e-1 * meyasu_x
+meyasu_y3 = 1e-2 * meyasu_x
+
+# ax1.plot(meyasu_x, meyasu_y0, c = "C2", linestyle=":", label=r"$r^4 = 10 h$")
+ax1.plot(meyasu_x, meyasu_y1, c = "C3", linestyle=":", label=r"$r^4 = h$")
+ax1.plot(meyasu_x, meyasu_y2, c = "C4", linestyle=":", label=r"$r^4 = 0.1 h$")
+ax1.plot(meyasu_x, meyasu_y3, c = "C5", linestyle=":", label=r"$r^4 = 0.01 h$")
+
+# 凡例の表示
+ax1.legend(loc="upper left")
+
+# グラフの保存
+plt.savefig("graph.pdf")

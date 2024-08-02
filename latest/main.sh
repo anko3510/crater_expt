0bde7d725b33c877362bdb05e38be8e6cac87e4f
mkto <mkto@outlook.com>
2024-08-03 01:19:24 +0900

    update

--- latest/main.sh
+++ latest/main.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Windows のホームディレクトリを取得
-# W_HOME=$(pwd | sed -r 's#(([^/]*/){5}).*#\1#')
-if pwd | grep -q "OneDrive"; then
-    W_HOME=$(pwd | sed -r 's#(([^/]*/){6}).*#\1#')
-else
-    W_HOME=$(pwd | sed -r 's#(([^/]*/){5}).*#\1#')
-fi
-
 # main.shのディレクトリを取得
 HD=$(pwd)
 
@@ -20,7 +12,14 @@ mv graph.svg ./../pandoc/figures/graph.svg
 cd "$HD"
 
 # 撮影したクレーターの画像を ./tex/figures/crater.jpg にコピー
-NEWEST_JPG=$(ls -t "$W_HOME""Pictures/Camera Roll/"*.jpg | head -n1)
+if pwd | grep -q "OneDrive"; then
+    W_HOME=$(pwd | sed -r 's#(([^/]*/){6}).*#\1#')
+    NEWEST_JPG=$(ls -t "$W_HOME""画像/カメラ ロール/"*.jpg | head -n1)
+else
+    W_HOME=$(pwd | sed -r 's#(([^/]*/){5}).*#\1#')
+    NEWEST_JPG=$(ls -t "$W_HOME""Pictures/Camera Roll/"*.jpg | head -n1)
+fi
+
 cp "$NEWEST_JPG" ./pandoc/figures/crater.jpg
 
 # pandoc + firefox で印刷する PDF を作成
@@ -30,4 +29,4 @@ pandoc -s index.md -o index.html -d default.yaml --metadata-file=metadata.yaml
 cd "$HD"
 echo
 echo "生成した html"
-echo "$HD""pandoc/index.html"
+echo "$HD""/pandoc/index.html"
